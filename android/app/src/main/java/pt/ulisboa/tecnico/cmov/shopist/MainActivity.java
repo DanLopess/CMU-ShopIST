ba62f6b48d36e0c86527b07878b5b41665365cec
ist190637 <xavier.gomes@tecnico.ulisboa.pt>
2021-03-29 21:04:14 +0100

    Main screen layout almost done, needs logic to create lists and store them

--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
@@ -1,8 +1,13 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //load local lists
+        //if wifi available sync lists
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        TextView titleTextView = findViewById(R.id.textView_title);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    titleTextView.setText(item.getTitle());
+                    //update shown lists
+                    return false;
+                }
+        );
+
+        // Select initial list TODO based on location
+        //for now select Pantry list
+        bottomNavigationView.setSelectedItemId(R.id.action_pantry_lists);
     }
 }
