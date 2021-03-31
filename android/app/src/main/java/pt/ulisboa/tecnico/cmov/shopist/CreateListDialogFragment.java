2e4c55f65a6edf2884c158ea531274957bcb0d66
ist190637 <xavier.gomes@tecnico.ulisboa.pt>
2021-03-31 17:51:08 +0100

    Dialog layout base finished, now it needs the logic to actually create the lists

---
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/CreateListDialogFragment.java
@@ -0,0 +1,74 @@
+package pt.ulisboa.tecnico.cmov.shopist;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
+
+public class CreateListDialogFragment extends DialogFragment {
+    private final Context context;
+
+    public CreateListDialogFragment(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_create_list, null);
+
+        Spinner spinnerCategory = dialogView.findViewById(R.id.spinner_category);
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(context,
+                R.array.available_categories, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(categoryAdapter);
+
+        Spinner spinnerLocation = dialogView.findViewById(R.id.spinner_location);
+        ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(context,
+                R.array.location_options, android.R.layout.simple_spinner_item);
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLocation.setAdapter(locationAdapter);
+
+        spinnerLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Button btn = dialogView.findViewById(R.id.button_pickLocation);
+                if (position == 2 && btn.getVisibility() != View.VISIBLE)
+                    btn.setVisibility(View.VISIBLE);
+                else if (btn.getVisibility() != View.GONE)
+                    btn.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setTitle(R.string.create_list_dialog_title)
+                .setView(dialogView)
+                .setPositiveButton(R.string.create_ok, (dialog, id) -> {
+                    // Reaction to OK button press
+                    // TODO Get all the data necessary
+                    // String listName = getActivity().findViewById(R.id.editText_listName).toString();
+                    // Create the list object
+                })
+                .setNegativeButton(R.string.cancel, (dialog, id) -> Objects.requireNonNull(CreateListDialogFragment.this.getDialog()).cancel());
+        // Create the dialog and return it
+        return builder.create();
+    }
+}
--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
@@ -1,7 +1,7 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private String currCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 item -> {
-                    titleTextView.setText(item.getTitle());
+                    currCategory = item.getTitle().toString(); // To set the default for the list creation
+                    titleTextView.setText(currCategory);
                     //update shown lists
                     return false;
                 }
@@ -33,5 +35,15 @@ public class MainActivity extends AppCompatActivity {
         // Select initial list TODO based on location
         //for now select Pantry list
         bottomNavigationView.setSelectedItemId(R.id.action_pantry_lists);
+
+    }
+
+    // TODO call to the dialog
+    // In order to have a pre-selected value displayed, you can call setText(CharSequence text, boolean filter) on the AutoCompleteTextView with the filter set to false.
+    // Set Category default to current category
+    // Set location default to use current location, other options are none or pick location on map
+    public void createNewList(MenuItem item) {
+        DialogFragment createListDialog = new CreateListDialogFragment(this);
+        createListDialog.show(getSupportFragmentManager(), "create list");
     }
 }
