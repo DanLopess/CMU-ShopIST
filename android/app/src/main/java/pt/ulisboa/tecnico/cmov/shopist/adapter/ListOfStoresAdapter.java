eaab399e1c7cbaf272ddfa3f8eaadc093b3d7192
ist190637 <xavier.gomes@tecnico.ulisboa.pt>
2021-05-18 12:15:25 +0100

    fix

--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/adapter/ListOfStoresAdapter.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/adapter/ListOfStoresAdapter.java
@@ -1,8 +1,11 @@
 package pt.ulisboa.tecnico.cmov.shopist.adapter;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
@@ -10,9 +13,14 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.List;
 
